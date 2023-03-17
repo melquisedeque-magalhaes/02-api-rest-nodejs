bcf09b9f73765550aba7783c73159ce301f9d08d
melquisedeque-magalhaes <melqui.sodre15@gmail.com>
2023-03-17 19:41:46 -0300

    add types knex

--- src/routes/transactionsRoutes.ts
+++ src/routes/transactionsRoutes.ts
@@ -1,10 +1,24 @@
+import { randomUUID } from 'node:crypto'
 import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
 import { knex } from '../database'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/hello', async () => {
-    const tables = await knex('sqlite_schema').select('*')
+  app.post('/', async (request, reply) => {
+    const bodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
 
-    return tables
+    const { amount, title, type } = bodySchema.parse(request.body)
+
+    await knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    return reply.status(201).send()
   })
 }
--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,9 @@ import { transactionsRoutes } from './routes/transactionsRoutes'
 
 const app = fastify()
 
-app.register(transactionsRoutes)
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
+})
 
 app
   .listen({
